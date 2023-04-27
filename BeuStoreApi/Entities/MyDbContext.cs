198a550cea3bfa84ba1eaaa7d981cd2e22482602
Trần Long <64456550+long2707@users.noreply.github.com>
2023-04-27 17:43:00 +0700

    update database

--- BeuStoreApi/Entities/MyDbContext.cs
+++ BeuStoreApi/Entities/MyDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace BeuStoreApi.Entities
 {
@@ -9,9 +10,23 @@ namespace BeuStoreApi.Entities
         {
 
         }
+         public DbSet<Products> products { get; set; }
+        public DbSet<Categories> categories { get; set; } 
+        public DbSet<Gallerles> gallerles { get; set; }
+        public DbSet<Tags> tags { get; set; }
+        public DbSet<Attrbutes> attributes { get; set; }
+        public DbSet<AttrbuteValue> attributesValue { get; set; }
+        public DbSet<Variants> variants { get; set; }
+        public DbSet<VariantValues> variantValues { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Categories>()
+                .HasMany(c => c.Children)
+                .WithOne(c => c.Parent)
+                .HasForeignKey(c => c.parent_id);
+
         }
     }
 }
--- BeuStoreApi/Migrations/20230427103913_create-db.Designer.cs
+++ BeuStoreApi/Migrations/20230427103913_create-db.Designer.cs
@@ -4,6 +4,7 @@ using BeuStoreApi.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BeuStoreApi.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230427103913_create-db")]
+    partial class createdb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
